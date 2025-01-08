795eb01cc7e4459f4765ececc1863e02738d3f72
mnguyen175 <maryannguyen2017@gmail.com>
2025-01-08 21:03:37 +0000

    day 1

--- classwork.cpp
+++ classwork.cpp
@@ -8,6 +8,13 @@ void displayList(std::vector<T> const &v);
 int main()
 {
     // You can follow along with any code in class here
+    int x = 5;
+    double y = 3.14;
+    std::string name = "Mr. Vu";
+
+    std::vector<int> jennys = [8, 6, 7, 5, 3, 0, 9];
+    std::vector<double> foo = {3.14, 2.5};
+    std::vector<std::string> bar = {"hello", "darkness", "my", "old", "friend"};
 
     
     return 0;
